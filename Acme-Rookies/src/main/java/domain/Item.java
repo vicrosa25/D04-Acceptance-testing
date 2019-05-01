5d1c08032769eae7d1b79dd40007474f1961d354
Víctor Rosa <vicrosa25@gmail.com>
2019-05-01 13:41:55 +0200

    Add populate Providers and Items, Update Item link, #8

--- Acme-Rookies/src/main/java/domain/Item.java
+++ Acme-Rookies/src/main/java/domain/Item.java
@@ -4,12 +4,12 @@ import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 @Entity
 @Access(AccessType.PROPERTY)
@@ -18,7 +18,7 @@ public class Item extends DomainEntity {
 	// Attributes ------------------------------------------------------------------------------------------
 	private String 				name;
 	private String 				description;
-	private Url					link;
+	private String				link;
 	private Collection<Url>		pictures;
 	
 	
@@ -40,17 +40,17 @@ public class Item extends DomainEntity {
 		this.description = description;
 	}
 	
-	@NotNull
-	@Valid
-	public Url getLink() {
+	@NotBlank
+	@URL
+	public String getLink() {
 		return link;
 	}
 	
-	public void setLink(Url link) {
+	public void setLink(String link) {
 		this.link = link;
 	}
 	
-	
+	@ElementCollection
 	public Collection<Url> getPictures() {
 		return pictures;
 	}
