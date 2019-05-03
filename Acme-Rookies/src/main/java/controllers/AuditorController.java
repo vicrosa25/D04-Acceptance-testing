f77a23736747c7b72d2511741d485b242eeb3f5c
Víctor Rosa <vicrosa25@gmail.com>
2019-05-03 10:50:00 +0200

    Add Auditor Controller and Service, #5

---
+++ Acme-Rookies/src/main/java/controllers/AuditorController.java
@@ -0,0 +1,98 @@
+
+package controllers;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+import javax.validation.Valid;
+
+import org.hibernate.TypeMismatchException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import domain.Auditor;
+import services.AuditorService;
+import utilities.Md5;
+
+@Controller
+@RequestMapping("/auditor")
+public class AuditorController {
+
+	@Autowired
+	private AuditorService auditorService;
+
+
+	@ExceptionHandler(TypeMismatchException.class)
+	public ModelAndView handleMismatchException(final TypeMismatchException oops) {
+		JOptionPane.showMessageDialog(null, "Forbidden operation");
+		return new ModelAndView("redirect:/");
+	}
+
+	// List -------------------------------------------------------------
+	@RequestMapping(value = "/admin/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView result;
+		Collection<Auditor> auditors;
+
+		auditors = this.auditorService.findAll();
+
+		result = new ModelAndView("auditor/admin/list");
+		result.addObject("auditors", auditors);
+		result.addObject("requestURI", "auditor/admin/list.do");
+
+		return result;
+	}
+
+	// Create -----------------------------------------------------------
+	@RequestMapping(value = "/admin/create", method = RequestMethod.GET)
+	public ModelAndView create() {
+		ModelAndView result;
+		Auditor auditor;
+
+		auditor = this.auditorService.create();
+
+		result = new ModelAndView("auditor/admin/create");
+		result.addObject("auditor", auditor);
+
+		return result;
+	}
+
+	// Save -----------------------------------------------------------
+	@RequestMapping(value = "/create", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid Auditor auditor, BindingResult binding) {
+		ModelAndView result;
+		String password;
+		if (binding.hasErrors()) {
+			final List<ObjectError> errors = binding.getAllErrors();
+			for (final ObjectError e : errors)
+				System.out.println(e.toString());
+			result = new ModelAndView("auditor/create");
+			result.addObject("auditor", auditor);
+		} else
+			try {
+				password = Md5.encodeMd5(auditor.getUserAccount().getPassword());
+				auditor.getUserAccount().setPassword(password);
+				this.auditorService.save(auditor);
+				result = new ModelAndView("redirect:list.do");
+			} catch (final Throwable oops) {
+				result = new ModelAndView("auditor/admin/create");
+				result.addObject("auditor", auditor);
+				if (oops instanceof DataIntegrityViolationException)
+					result.addObject("auditor", "admin.duplicated.username");
+				else {
+					System.out.println(oops.getCause().toString());
+					result.addObject("auditor", "admin.registration.error");
+				}
+			}
+		return result;
+	}
+
+}
--- Acme-Rookies/src/main/java/services/AuditorService.java
+++ Acme-Rookies/src/main/java/services/AuditorService.java
@@ -1,15 +1,20 @@
 
 package services;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
+import domain.Auditor;
+import domain.Message;
 import repositories.AuditorRepository;
+import security.Authority;
 import security.LoginService;
 import security.UserAccount;
-import domain.Auditor;
 
 @Service
 @Transactional
@@ -23,22 +28,58 @@ public class AuditorService {
 
 
 	// CRUD methods
+	public Auditor create() {
+
+		Auditor auditor = new Auditor();
+
+		// Initialice
+		UserAccount userAccount = new UserAccount();
+		Collection<Authority> authorities = new ArrayList<Authority>();
+		Authority authority = new Authority();
+		authority.setAuthority(Authority.AUDITOR);
+		authorities.add(authority);
+		userAccount.setAuthorities(authorities);
+
+		
+		// Set Messages
+		Collection<Message> messages = new ArrayList<Message>();
+
+		auditor.setUserAccount(userAccount);
+		auditor.setMessages(messages);
+		auditor.setIsSpammer(false);
+
+		return auditor;
+
+	}
+	
+	
 	public Auditor findOne(final int auditorId) {
 		final Auditor result = this.auditorRepository.findOne(auditorId);
 		Assert.notNull(result);
 
 		return result;
 	}
+	
+	public Collection<Auditor> findAll() {
+		Collection<Auditor> result = this.auditorRepository.findAll();
+		Assert.notNull(result);
 
-	public Auditor save(final Auditor auditor) {
+		return result;
+	}
+	
+	
+
+	public Auditor save(Auditor auditor) {
 		Assert.notNull(auditor);
 
 		final Auditor result = this.auditorRepository.save(auditor);
 
 		return result;
 	}
+	
+	
+	
 	/************************************************************************************************/
-
 	// Other business methods
 	public Auditor findByPrincipal() {
 		Auditor result;
