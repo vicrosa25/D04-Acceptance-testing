8a4f977caf6fbca5de1333939a89a73d3770b0f8
Martin <vicmardp2.d02@gmail.com>
2019-05-03 14:33:08 +0200

    #13 create/edit

--- Acme-Rookies/src/main/java/services/AuditService.java
+++ Acme-Rookies/src/main/java/services/AuditService.java
@@ -1,13 +1,17 @@
 
 package services;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
 import repositories.AuditRepository;
+import domain.Actor;
 import domain.Audit;
+import domain.Auditor;
 
 @Service
 @Transactional
@@ -18,12 +22,53 @@ public class AuditService {
 	private AuditRepository		auditRepository;
 
 	// Supporting services
+	@Autowired
+	private ActorService		actorService;
+	@Autowired
+	private AuditorService		auditorService;
 
 
 	// CRUD methods
+	public Audit create() {
+		Audit result;
+		Actor principal;
+
+		result = new Audit();
+
+		// Principal must be a Company
+		principal = this.actorService.findByPrincipal();
+		Assert.isInstanceOf(Auditor.class, principal);
+
+		result.setMoment(new Date());
+		result.setFinalMode(false);
+
+		return result;
+	}
+
 	public Audit findOne(final int auditId) {
 		final Audit result = this.auditRepository.findOne(auditId);
 		Assert.notNull(result);
+
+		return result;
+	}
+
+	public Audit save(final Audit audit) {
+		boolean nuevo = false;
+		Assert.notNull(audit);
+		Auditor principal = this.auditorService.findByPrincipal();
+
+		if(audit.getId()==0){
+			nuevo = true;
+		}else{
+			Assert.isTrue(principal.getAudits().contains(audit));
+		}
+
+		final Audit result = this.auditRepository.save(audit);
+
+		if (nuevo){
+			principal.getAudits().add(result);
+			result.getPosition().getAudits().add(result);
+		}
 
 		return result;
 	}
